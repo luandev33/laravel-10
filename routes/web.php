d1050bb9dc73f9d638cffa8d2e4a29e502ca3877
Luan Rodrigues <lurh17185@gmail.com>
2024-11-21 21:47:49 -0300

    referenciando rota

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+ 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+ 
+Route::prefix('produtos')->group( function() {
+    Route::get('/', [ProdutoController::class, 'index'])->name('produto.index');
+    
 });
